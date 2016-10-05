c326d858709f5ee1c229c0fc6bb756715080cc7a
Kurt Riede <kriede@users.noreply.github.com>
2016-10-06 00:19:40 +0200

    fixed cut&paste error in function navigateToProject

--- src/home/home.component.ts
+++ src/home/home.component.ts
@@ -36,7 +36,7 @@ export class HomeComponent extends BaseComponent {
         this._navigateCommon("onenote");
     }
 
-    navigateToOneNote() {
+    navigateToProject() {
         this._navigateCommon("project");
     }
 
